8f49aab21a5487c21a39009bba4f1ee925ef6b61
Markus Faerevaag <m@frv.ag>
2016-11-29 16:59:18 +0100

    report: refactor and begin risk analyssi

--- src/client/main.rs
+++ src/client/main.rs
@@ -12,6 +12,36 @@ use utils::hexdump;
 use engine::Engine;
 
 fn main () {
+    let mut key = PKey::from_rsa(Rsa::generate(2048).unwrap()).unwrap();
+    let mut rsa = key.rsa().unwrap();
+
+
+    let s = String::from("SUCH S3CRET");
+    let message = s.as_bytes();
+    let mut cipher: [u8; 256] = [0; 256];
+    let mut result: [u8; 256] = [0; 256];
+
+    let mut stdout = io::stdout();
+
+    // display the private key.
+    println!("public key:");
+    hexdump(&mut stdout, &rsa.private_key_to_pem().unwrap());
+
+    println!("message: {}", String::from_utf8_lossy(message));
+    println!("bytes[{}]: {:?}", &message.len(), &message);
+
+    // encrypt the string and display the ciphertext.
+    let res = rsa.private_encrypt(&message, &mut cipher, PKCS1_PADDING).unwrap();
+    println!("encrypted: [{}]", res);
+    hexdump(&mut stdout, &cipher);
+
+    // Decrypt the string and show the plaintext.
+    let res = rsa.public_decrypt(&cipher, &mut result, PKCS1_PADDING).unwrap();
+    println!("decrypted (hex):");
+    hexdump(&mut stdout, &result);
+    println!("decrypted (string): \"{}\"", str::from_utf8(&result).unwrap());
+
+
     let engine = Engine::new("ws://127.0.0.1:10000");
 
     engine.run();
